903199cfc992f67fb883139ab8e1f308aaf0c71b
Nikolay Gorohovsky <fi1a@icloud.com>
2023-01-03 06:30:51 +0500

    Initial commit

---
+++ tests/bxdefine.php
@@ -0,0 +1,8 @@
+<?php
+
+declare(strict_types=1);
+
+define('NO_KEEP_STATISTIC', true);
+define('NOT_CHECK_PERMISSIONS', true);
+define('BX_WITH_ON_AFTER_EPILOG', true);
+define('BX_NO_ACCELERATOR_RESET', true);
